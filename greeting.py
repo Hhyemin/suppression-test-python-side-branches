073f1c761cdfe48e790a33118824344f9977e439
Huimin <huhuimin236@gmail.com>
2023-09-27 14:26:08 +0200

    Remove the never used code (one way to fix the warning)

--- greeting.py
+++ greeting.py
@@ -1,19 +1,10 @@
 def greet(name):
     print(f"Hello, {name}!")
 
-def capitalize_string(s):
-    return s.upper()
-
 def main():
     person = "Alice"
     greeting = greet(person)
-
-    message = "this is a test"
-    # pylint: disable=unused-variable
-    capitalized_message = capitalize_string(message)
-
     print(greeting)
 
 if __name__ == "__main__":
     main()
-
